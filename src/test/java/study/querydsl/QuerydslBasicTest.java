ca30ec281acfb099f1e87da171a84713b9575903
arcjjang <arcelias@naver.com>
2022-09-27 15:32:18 +0900

    실전! Querydsl - 중급 문법 : 프로젝션과 결과 반환 - @QueryProjection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.dto.MemberDto;
+import study.querydsl.dto.QMemberDto;
 import study.querydsl.dto.UserDto;
 import study.querydsl.entity.Member;
 import study.querydsl.entity.QMember;
@@ -544,6 +545,18 @@ public class QuerydslBasicTest {
     }
 
     @Test
+    public void distinct() {
+        List<String> result = queryFactory
+                .select(member.username).distinct()
+                .from(member)
+                .fetch();
+
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
     public void simpleProjection() {
         List<String> result = queryFactory
                 .select(member.username)
@@ -667,6 +680,18 @@ public class QuerydslBasicTest {
 
         for (UserDto userDto : result) {
             System.out.println("UserDto = " + userDto);
+        }
+    }
+
+    @Test
+    public void findDtoByQueryProjection() {
+        List<MemberDto> result = queryFactory
+                .select(new QMemberDto(member.username, member.age))
+                .from(member)
+                .fetch();
+
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto = " + memberDto);
         }
     }
 
